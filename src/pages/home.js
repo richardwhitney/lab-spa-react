1b85c643ae7ead374cb654be99d4a7700e8edf8b
Richard Whitney <rwhitney32@gmail.com>
2020-05-11 19:44:07 +0100

    Added tool tips for home page

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React, {Component} from "react";
-import {Grid, Segment} from "semantic-ui-react";
+import {Grid, Segment, Popup} from "semantic-ui-react";
 import {Link} from "react-router-dom";
 
 class Home extends Component{
@@ -11,100 +11,127 @@ class Home extends Component{
             textAlign='center'>
         <Grid.Row stretched>
           <Grid.Column>
-            <Segment as={Link}
-                     to={'/testhub'}
-                     inverted color='red'
-                     padded style={{marginTop: '1em'}}
-                     size='massive'
-            >
-              Test Guide
-            </Segment>
+            <Popup content='Test hub tool tip' trigger={
+              <Segment as={Link}
+                       to={'/testhub'}
+                       inverted color='red'
+                       padded style={{marginTop: '1em'}}
+                       size='massive'
+              >
+                Test Guide
+              </Segment>}
+            />
+
           </Grid.Column>
           <Grid.Column>
-            <Segment as={Link}
-                     to={'/contacts'}
-                     inverted color='orange'
-                     padded style={{marginTop: '1em'}}
-                     size='massive'
-            >
-              Contacts
-            </Segment>
+            <Popup content='Contact tool tip' trigger={
+              <Segment as={Link}
+                       to={'/contacts'}
+                       inverted color='orange'
+                       padded style={{marginTop: '1em'}}
+                       size='massive'
+              >
+                Contacts
+              </Segment>}
+            />
+
           </Grid.Column>
           <Grid.Column>
-            <Segment as={Link}
-                     to={'/clinicalPathwaysHub'}
-                     inverted color='yellow'
-                     padded style={{marginTop: '1em'}}
-                     size='massive'
-            >
-              Clinical Pathways
-            </Segment>
+            <Popup content='Clinical pathways tooltip' trigger={
+              <Segment as={Link}
+                       to={'/clinicalPathwaysHub'}
+                       inverted color='yellow'
+                       padded style={{marginTop: '1em'}}
+                       size='massive'
+              >
+                Clinical Pathways
+              </Segment>
+            }
+            />
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row stretched>
           <Grid.Column>
-            <Segment as={Link}
-                     to={'/haematology'}
-                     inverted color='olive'
-                     padded style={{marginTop: '1em'}}
-                     size='massive'
-            >
-              Haematology
-            </Segment>
+            <Popup content='Haematology tool tip' trigger={
+              <Segment as={Link}
+                       to={'/haematology'}
+                       inverted color='olive'
+                       padded style={{marginTop: '1em'}}
+                       size='massive'
+              >
+                Haematology
+              </Segment>
+            }
+            />
           </Grid.Column>
           <Grid.Column>
-            <Segment as={Link}
-                     to={'/biochemistry'}
-                     inverted color='green'
-                     padded style={{marginTop: '1em'}}
-                     size='massive'
-            >
-              Biochemistry
-            </Segment>
+            <Popup content='Biochemistry tool tip' trigger={
+              <Segment as={Link}
+                       to={'/biochemistry'}
+                       inverted color='green'
+                       padded style={{marginTop: '1em'}}
+                       size='massive'
+              >
+                Biochemistry
+              </Segment>
+            }
+            />
           </Grid.Column>
           <Grid.Column>
-            <Segment as={Link}
-                     to={'/bloodTransfusion'}
-                     inverted color='teal'
-                     padded style={{marginTop: '1em'}}
-                     size='massive'
-            >
-              Blood Transfusion
-            </Segment>
+            <Popup content='Blood transfusion tool tip' trigger={
+              <Segment as={Link}
+                       to={'/bloodTransfusion'}
+                       inverted color='teal'
+                       padded style={{marginTop: '1em'}}
+                       size='massive'
+              >
+                Blood Transfusion
+              </Segment>
+            }
+            />
           </Grid.Column>
         </Grid.Row>
 
         <Grid.Row stretched>
           <Grid.Column>
-            <Segment as={Link}
-                     to={'/bloodProductInformation'}
-                     inverted color='blue'
-                     padded style={{marginTop: '1em'}}
-                     size='massive'
-            >
-              Blood Product Information
-            </Segment>
+            <Popup content='Blood product information tool tip' trigger={
+              <Segment as={Link}
+                       to={'/bloodProductInformation'}
+                       inverted color='blue'
+                       padded style={{marginTop: '1em'}}
+                       size='massive'
+              >
+                Blood Product Information
+              </Segment>
+            }
+            />
           </Grid.Column>
           <Grid.Column>
-            <Segment as={Link}
-                     to={'/news'}
-                     inverted color='violet'
-                     padded style={{marginTop: '1em'}}
-                     size='massive'
-            >
-              News Feed
-            </Segment>
+            <Popup content='News Feed tool tip' trigger={
+              <Segment as={Link}
+                       to={'/news'}
+                       inverted color='violet'
+                       padded style={{marginTop: '1em'}}
+                       size='massive'
+              >
+                News Feed
+              </Segment>
+            }
+            />
           </Grid.Column>
           <Grid.Column>
-            <Segment as={Link}
-                     to={'/quizhub'}
-                     inverted color='purple'
-                     padded style={{marginTop: '1em'}}
-                     size='massive'
-            >
-              Quiz Hub
-            </Segment>
+            <Popup content='Quiz hub tool tip' trigger={
+              <Segment as={Link}
+                       to={'/quizhub'}
+                       inverted color='purple'
+                       padded style={{marginTop: '1em'}}
+                       size='massive'
+              >
+                Quiz Hub
+              </Segment>
+            }
+            />
           </Grid.Column>
         </Grid.Row>
 
